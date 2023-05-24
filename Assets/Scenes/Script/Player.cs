c3e3243cee8d6ccc88bcf2b981658123fcf22158
k022g0028@g.neec.ac.jp <k022g0028@g.neec.ac.jp>
2023-05-24 14:30:56 +0900

    a

--- Assets/Scenes/Script/Player.cs
+++ Assets/Scenes/Script/Player.cs
@@ -2,7 +2,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
